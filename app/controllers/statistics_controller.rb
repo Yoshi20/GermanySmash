4558f9fcca9e1d00ee06a2a115735c36eed1cbe0
Jascha Haldemann <jh@oxon.ch>
2022-02-26 14:49:32 +0100

    @player_count_total = @players.count

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -5,7 +5,7 @@ class StatisticsController < ApplicationController
   # GET /statistics.json
   def index
     @players = Player.all_de
-    @player_count_total = @players.count - 4 # do not count bye0, bye1 bye2 & bye3
+    @player_count_total = @players.count
     # @player_count_2022 = @players.from_2022.count
   end
 
